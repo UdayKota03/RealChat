79dd73ec399fc3bdaaf050581f9b6f84204b533c
UdayKota03 <udaykota4u@gmail.com>
2024-10-06 00:29:31 +0530

    final update

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -14,7 +15,7 @@ import { app,server } from './socket/socket.js';
 
 const PORT = process.env.PORT || 8000;
 
-
+const __dirname =path.resolve();
 
 
 app.use(express.json());  // to parse the incoming requests with JSON payloads (from req.body)
@@ -29,14 +30,21 @@ app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+
 app.use(cors({
     origin: 'http://localhost:3000', // Your frontend origin
     credentials: true, // Allow cookies to be sent
 }));
 
-app.get("/",(req,res)=>{
-    res.send("hello")
-})
+// app.get("/",(req,res)=>{
+//     res.send("hello")
+// })
 
 
 server.listen(PORT, () => {
